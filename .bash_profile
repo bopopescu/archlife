07879dd522bf03c5c9e5a473dcf1f21ac360c55c
Zernach <ryan@zernach.com>
2020-02-03 12:01:15 -0500

    firebase

---
+++ .bash_profile
@@ -0,0 +1,6 @@
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/lambda_school_loaner_217/lambda/archlife/google-cloud-sdk/path.bash.inc' ]; then . '/Users/lambda_school_loaner_217/lambda/archlife/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/lambda_school_loaner_217/lambda/archlife/google-cloud-sdk/completion.bash.inc' ]; then . '/Users/lambda_school_loaner_217/lambda/archlife/google-cloud-sdk/completion.bash.inc'; fi
--- pages/index.py
+++ pages/index.py
@@ -5,9 +5,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
-
-# Imports from this application
+# Imports from this application:
 from app import app
+#Imports from Google Cloud & Firebase:
+import firebase_admin
+from firebase_admin import credentials
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
@@ -21,6 +23,9 @@ column1 = dbc.Col(
         dcc.Link(dbc.Button("LOGIN (COMING SOON)  ➡️", color='primary'), href='/browsing_history'),
         html.Br(),
         html.Br(),
+
+        #cred = credentials.Certificate("path/to/serviceAccountKey.json")
+        #firebase_admin.initialize_app(cred)
     ],
     md=6
 )
@@ -28,7 +33,7 @@ column1 = dbc.Col(
 column2 = dbc.Col(
     [
         #dcc.Graph(figure=fig)
-        html.Img(src='assets/Archlife-Intro-App-Logo-Dark.png', className='img-fluid', height=300, width=300)
+        html.Img(src='assets/Archlife-Intro-App-Logo-Dark.png', className='img-fluid', height=200, width=200)
     ],
     md=6
 )
